a1e2b6b7651ff72762c600ec61176f1b35b04599
Prakrity Darnal <prakritydarnal428@gmail.com>
2025-11-27 10:06:49 +0545

    Task2

---
+++ task2/Motorcycle.cs
@@ -0,0 +1,9 @@
+public class Motorcycle : Vehicle
+{
+    public bool HasCarrier { get; set; }
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Brand: {Brand}, Speed: {Speed} km/h, Carrier: {HasCarrier}");
+    }
+}
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace task2;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Car object
+        Car car = new Car
+        {
+            Brand = "Toyota",
+            Speed = 180,
+            Seats = 5
+        };
+
+        car.Start();
+        car.DisplayInfo();
+        car.Stop();
+
+        Console.WriteLine();
+
+        // Motorcycle object
+        Motorcycle bike = new Motorcycle
+        {
+            Brand = "Yamaha",
+            Speed = 120,
+            HasCarrier = true
+        };
+
+        bike.Start();
+        bike.DisplayInfo();
+        bike.Stop();
     }
 }
